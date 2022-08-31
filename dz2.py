d1f6efa9ba36e1305f2e445ec5b6eb1a4e2c265e
1 <cccamboy75@gmail.com>
2022-08-31 23:42:01 +0300

    Initial commit

--- dz2.py
+++ dz2.py
@@ -1,14 +1,16 @@
-n = input("Введите 4-ох значное число: ")
-n = int(n)
-d1 = n % 10
-n = n // 10
-d2 = n % 10
-n = n // 10
-d3 = n % 10
-n = n // 10
-d4 = n % 10
-n = n // 10
-print(d4)
-print(d3)
-print(d2)
-print(d1)
+x = input("Введите 4-ох значное число: ")
+x = int(x)
+
+n1 = x % 10
+x = x // 10
+n2 = x % 10
+x = x // 10
+n3 = x % 10
+x = x // 10
+n4 = x % 10
+x = x // 10
+
+print(n4)
+print(n3)
+print(n2)
+print(n1)
