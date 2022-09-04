3ecc2d78434b276ad76dfffc105396834e7de4c8
1 <cccamboy75@gmail.com>
2022-09-04 12:07:47 +0300

    Initial commit

--- venv/dz3kalkyltr.py
+++ venv/dz3kalkyltr.py
@@ -1,20 +1,15 @@
-print("Выберете действие")
-if True:
-    s = input("Введите знак (+,-,*,/): ")
-    if s in ('+', '-', '*', '/'):
-        x = float(input("x="))
-        y = float(input("y="))
-        if s == '+':
-            print (x+y)
-        elif s == '-':
-            print(x-y)
-        elif s == '*':
-            print(x*y)
-        elif s == '/':
-            if y != 0:
-                print (x/y)
-            else:
-                print("Деление на ноль запрещенно!")
+x = float(input('Введите число '))
+s = input('Выберите действие + , - , * , / ')
+y = float(input('Введите число '))
+if s == '+':
+    print (x+y)
+elif s == '-':
+     print(x-y)
+elif s == '*':
+    print(x*y)
+elif s == '/':
+    if y != 0:
+         print (x/y)
     else:
-        print("Неверный знак операции!")
+       print("Деление на ноль запрещенно!")
 
