62bc28049001c5c1e37198ccc69df4d3a5035ce6
1 <cccamboy75@gmail.com>
2022-09-08 22:52:02 +0300

    Initial commit

--- venv/dz7.py
+++ venv/dz7.py
@@ -4,6 +4,5 @@ a = [1, 0, 3, 0, 0, 0, 5]
 #a = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
 for i in range(len(a) - 1, -1, -1):
     if a[i] == 0:
-        print(i)
         a.append(a.pop(i))
 print(a)
