d0650dd5e98c6153d9d351f325e08a3d3eb2e0ec
1 <cccamboy75@gmail.com>
2022-09-08 19:15:45 +0300

    Initial commit

--- venv/dzspisok.py
+++ venv/dzspisok.py
@@ -9,6 +9,20 @@ else:
     spisok2.extend([spisok1[:y + 1], spisok1[y + 1:]])
     print(spisok1, " => ", spisok2)
     print(y)
-
+    # y = len(x)  len функция определяет колличество элементов списка
+    # если нужно удалить del(название списка[индекс])
+    # pop удаляет последний элемент из списка и запоминает его х.pop()
+    # x.insert(0, n)  insert добавляет элемент в сиписок x нужный список
+    # exntnd расширяет создает список пример:
+    myList = ["Lion", "Tiger", "Bear", "Cheetah", "Puma"]
+    listToAdd = ["Leopard", "Lynx"]
+     myList.extend(listToAdd)
+    print(myList)
+    ['Lion', 'Tiger', 'Bear', 'Cheetah', 'Puma', 'Leopard', 'Lynx']
+    ### добавляем строку в список
+    myList = ['X', 'Y', 'Z']
+    myList.extend('abcd')
+    print(myList)
+    # ['X', 'Y', 'Z', 'a', 'b', 'c', 'd']
 
 
