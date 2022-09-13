1234f490484d36d083e788bfc301b59c3e8812ec
1 <cccamboy75@gmail.com>
2022-09-13 20:26:17 +0300

    Initial commit

---
+++ venv/dz-12.py
@@ -0,0 +1,18 @@
+#a = 'Python Community' #-> #PythonCommunity
+
+#a = 'i like python community!' #-> #ILikePythonCommunity
+
+a = 'Should, I. subscribe? Yes!' #-> #ShouldISubscribeYes
+
+import string
+string.punctuation
+
+for p in string.punctuation:
+    if p in a:
+        a = a.replace(p, '')    #замена символа в строке
+
+b = a.split()                   # разбиваем строку на подстроки
+c = ''.join(b)                   # объединяем строку
+x = '#'
+
+print(x + c)
