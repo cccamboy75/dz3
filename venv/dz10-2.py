14bd8af219afcf7e0f98a2c353656d3947f2066b
1 <cccamboy75@gmail.com>
2022-09-18 22:26:48 +0300

    Initial commit

--- venv/dz10-2.py
+++ venv/dz10-2.py
@@ -1,21 +1,29 @@
 import keyword
 import string
 
-x = input('введите имя')
-a = len(x)
+w = ('_') #=> True
+#w = ('x') #=> True
+#w = ('get_value') #=> True
+#w = ('Get_value') #=> False
+#w = ('get_Value') #=> False
+#w = ('getValue') #=> False
+#w = ('3m') #=> False
+
+#w = input('введите имя')
+a = len(w)
 y = True
-if x.count('_') > 1:
+if w.count('_') > 1:
     y = False
-if x.isdigit():
+if w.isdigit():
     y = False
-if x[0] in string.digits:
+if w[0] in string.digits:
     y = False
-if x in keyword.kwlist:
+if w in keyword.kwlist:
     y = False
 
 for i in range(a):
-    if x[i] in string.ascii_uppercase:
-        okvar = False
-    elif x[i] in string.punctuation and '_' not in x[i]:
+    if w[i] in string.ascii_uppercase:
+        y = False
+    elif w[i] in string.punctuation and '_' not in w[i]:
         y = False
 print(y)
