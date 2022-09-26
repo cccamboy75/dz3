8dced43d163329eb4a9ebd00747543eaf1e168c6
1 <cccamboy75@gmail.com>
2022-09-26 19:07:11 +0300

    Initial commit

---
+++ venv/dz21.py
@@ -0,0 +1,14 @@
+import string
+string.punctuation
+
+def isPalindrome(x):
+    w =  x.lower()
+    b = w
+    for i in range(len(x)):
+        if x[i] in string.punctuation or x[i] in " ":
+            b = b.replace(x[i], "")
+    if b[::-1].startswith(b):
+        return True
+    else:
+        return False
+print(isPalindrome(input("Введите строку:")))
