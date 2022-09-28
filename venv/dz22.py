920c80c420a1040f1687c5d027c039c8d8bec916
1 <cccamboy75@gmail.com>
2022-09-28 23:32:26 +0300

    Initial commit

---
+++ venv/dz22.py
@@ -0,0 +1,9 @@
+def find_unique_value():
+    s = []
+    for i in numbers:
+       if numbers.count(i) == 1:
+          s.append(i)
+    return (s)
+
+numbers = [5, 5, 5, 0.5]
+print(find_unique_value())
