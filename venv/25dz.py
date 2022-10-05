0e31f0201bc915d8cd08f17cde81c6be20eb4348
1 <cccamboy75@gmail.com>
2022-10-05 18:02:16 +0300

    Initial commit

---
+++ venv/25dz.py
@@ -0,0 +1,9 @@
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    for i in range(end):
+        yield begin
+        begin = func(begin)
+        
+print(list(some_gen(2, 4, pow)))
